f98f2e24777e80fc52722303079c05c8d9c56030
Isaac Sudweeks <147561760+isaa-sudweeks@users.noreply.github.com>
2025-09-13 17:35:28 -0600

    Finished writing functions to calculate pawn moves

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -43,34 +43,17 @@ public class ChessPiece {
     public PieceType getPieceType() {
         return type;
     }
-
-    /**
-     * Calculates all the positions a chess piece can move to
-     * Does not take into account moves that are illegal due to leaving the king in
-     * danger
-     *
-     * @return Collection of valid moves
-     */
-    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        //We are going to start with pawns as they are relatively simple
-        //Paws have 3 types of movment. If they are on thier original square they can move forward 2
-        //They can always move forward 1 unless another piece is in the way
-        //They can move diagonally if they can take a piece
-
-        //Get position
-        int x = myPosition.getColumn();
-        int y = myPosition.getRow();
-
-        //Instantiate variable
+    public Collection<ChessMove> pawnMoves(ChessBoard board, ChessPosition myPosition) {
+        ChessPiece piece = board.getPiece(myPosition);
         List<ChessMove> moves = new ArrayList<>();
 
-        ChessPiece piece = board.getPiece(myPosition);
+        int x = myPosition.getColumn();
+        int y = myPosition.getRow();
+        PieceType promote = null;
         if (piece.getPieceType() == PieceType.PAWN){
             //White first
             if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
                 //Moving forward by 1 not promoting
-                //Check for promotion
-                PieceType promote = null;
                 if ((y+1) == 8){
                     promote = PieceType.QUEEN;
                 }
@@ -83,15 +66,53 @@ public class ChessPiece {
                 }
 
                 //Moving Diagonally to take
-                //First left direction non promotion
+                //First left direction
                 if (x != 1 && board.getPiece(new ChessPosition(y+1,x-1)) != null){
                     moves.add(new ChessMove(myPosition, new ChessPosition(y+1,x-1),promote));
                 }
-                if (x != 1 && board.getPiece(new ChessPosition(y+1,x+1)) != null){
+                //Right direction
+                if (x != 8 && board.getPiece(new ChessPosition(y+1,x+1)) != null){
                     moves.add(new ChessMove(myPosition, new ChessPosition(y+1,x+1),promote));
                 }
             }
+            else if (piece.getTeamColor() == ChessGame.TeamColor.BLACK){
+                if ((y-1) == 1){
+                    promote = PieceType.QUEEN;
+                }
+                if (board.getPiece(new ChessPosition(y-1, x)) == null) {
+                    moves.add(new ChessMove(myPosition, new ChessPosition(y - 1, x), promote));
+                }
+                if (y == 7 && board.getPiece(new ChessPosition(y-2,x))==null) {
+                    moves.add(new ChessMove(myPosition, new ChessPosition(y - 2, x), promote));
+                }
+                if (x != 1 && board.getPiece(new ChessPosition(y-1,x-1)) != null){
+                    moves.add(new ChessMove(myPosition, new ChessPosition(y-1,x-1),promote));
+                }
+                //Right direction
+                if (x != 8 && board.getPiece(new ChessPosition(y-1,x+1)) != null){
+                    moves.add(new ChessMove(myPosition, new ChessPosition(y-1,x+1),promote));
+                }
+            }
         }
+        return moves;
+    }
+
+
+    /**
+     * Calculates all the positions a chess piece can move to
+     * Does not take into account moves that are illegal due to leaving the king in
+     * danger
+     *
+     * @return Collection of valid moves
+     */
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+
+
+        //Instantiate variable
+        List<ChessMove> moves = new ArrayList<>();
+        moves.addAll(pawnMoves(board, myPosition));
+
+
 
         return moves;
     }
