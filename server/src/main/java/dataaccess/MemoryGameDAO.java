a385ece04ba7eb3dd76440e85e5a334c53a8dacf
Isaac Sudweeks <147561760+isaa-sudweeks@users.noreply.github.com>
2025-10-21 13:45:58 -0600

    Implemented MemoryGameDAO.java

---
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -0,0 +1,31 @@
+package dataaccess;
+
+import model.GameData;
+import model.UserData;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemoryGameDAO implements GameDAO{
+    private Map<String, GameData> games = new HashMap<>();
+    @Override
+    public List<GameData> listGames() {
+        return (List)games.values();
+    }
+
+    @Override
+    public void addGame(GameData gameData) { //Consider checking to see if the name has been used for.
+         games.put(gameData.gameName(),gameData);
+    }
+
+    @Override
+    public void updateGame(GameData gameData) {
+        games.replace(gameData.gameName(), gameData);
+    }
+
+    @Override
+    public void clear() {
+        games.clear();
+    }
+}
